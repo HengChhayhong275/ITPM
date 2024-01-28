a4313b629fc23829cef746185dba20fdb7349fcb
Heng Chhayhong <Henqchhayhong275@gmail.com>
2024-01-28 14:50:28 +0700

    create welcome page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        This is the login page
-    </div>
-</template>
+    <test>
+        <h1>hell</h1>
+        
+    </test>
+  </template>
+  
+  <script>
+    import test from '../layouts/testlayout.vue';
+  
+  export default {
+    components:{
+        test,
+    },
+    layout: 'testlayout'
+  };
+  </script>
+  <style scoped>
+    h1{
+        font-size: xx-large !important;
+        position: absolute !important;
+        color: black !important;
+        right: 50% !important;
+        top: 50% !important;
+    }
+  </style>
+  
